25bd6be27e0c1e246167285db18c3231fb975a0f
Jordan Baird <baird99.j@gmail.com>
2022-05-04 19:19:33 -0600

    Move `EventType` into `KeyEvent`

--- Sources/SwiftKeys/KeyEvent.swift
+++ Sources/SwiftKeys/KeyEvent.swift
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+import Carbon.HIToolbox
 import Cocoa
 
 public struct KeyEvent {
@@ -13,6 +14,43 @@ public struct KeyEvent {
     case key
     case modifiers
     case name
+  }
+  
+  /// Constants that specify the type of a key event.
+  ///
+  /// Pass these into a key event's `observe(_:handler:)` method. The closure you
+  /// provide in that method will be called whenever an event of this type is posted.
+  ///
+  /// ```swift
+  /// let event = KeyEvent(
+  ///     name: "Cheese",
+  ///     key: .leftArrow,
+  ///     modifiers: [.command, .option])
+  ///
+  /// event.observe(.keyDown) {
+  ///     print("KEY DOWN")
+  /// }
+  ///
+  /// event.observe(.keyUp) {
+  ///     print("KEY UP")
+  /// }
+  ///
+  /// // Note that `observe(_:handler:)` can be called multiple
+  /// // times, each invoking different closures.
+  /// ```
+  public enum EventType {
+    /// The key is released.
+    case keyUp
+    /// The key is pressed.
+    case keyDown
+    
+    init(_ eventRef: EventRef!) {
+      switch Int(GetEventKind(eventRef)) {
+      case kEventHotKeyPressed: self = .keyDown
+      case kEventHotKeyReleased: self = .keyUp
+      default: fatalError("Invalid event reference.")
+      }
+    }
   }
   
   static var keyEventStorage = [Name: KeyEvent]()
