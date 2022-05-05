449c9ee0a43c39bda612b473d714c359c6b4e937
Jordan Baird <baird99.j@gmail.com>
2022-05-05 16:09:40 -0600

    Update `generate-documentation` script

--- .github/scripts/generate-documentation.sh
+++ .github/scripts/generate-documentation.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+CHANGED_FILES=$(git status -s)
+
+# See if any files have changed. If so, stash them.
+if [[ $CHANGED_FILES ]]
+then
+  git stash
+fi
+
+# Generate and commit the docs.
 swift package \
   --allow-writing-to-directory ./docs \
     generate-documentation \
@@ -8,7 +17,11 @@ swift package \
       --transform-for-static-hosting \
       --hosting-base-path SwiftKeys \
       --output-path ./docs
-
-git checkout documentation
+      
 git commit -am "Update documentation"
-git push origin documentation
+
+# Reapply the stash, if needed.
+if [[ $CHANGED_FILES ]]
+then
+  git stash apply -q
+fi
