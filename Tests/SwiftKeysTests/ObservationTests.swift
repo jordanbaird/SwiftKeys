243cd43384e5e7a3c0286be3e8a727705b9220e2
Jordan Baird <baird99.j@gmail.com>
2022-05-15 11:29:54 -0600

    Add new test case

---
+++ Tests/SwiftKeysTests/ObservationTests.swift
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// ObservationTests.swift
+//
+// Created: 2022. Author: Jordan Baird.
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import SwiftKeys
+
+final class ObservationTests: XCTestCase {
+  var handler1WasCalled = false
+  var handler2WasCalled = false
+  lazy var o1 = KeyEvent.Observation(eventType: .keyDown) { self.handler1WasCalled = true }
+  lazy var o2 = KeyEvent.Observation(eventType: .keyDown) { self.handler2WasCalled = true }
+  
+  func testID() {
+    XCTAssertNotEqual(o1.id, o2.id)
+  }
+  
+  func testHandlers() {
+    XCTAssertFalse(handler1WasCalled)
+    XCTAssertFalse(handler2WasCalled)
+    o1.handler()
+    o2.handler()
+    XCTAssertTrue(handler1WasCalled)
+    XCTAssertTrue(handler2WasCalled)
+  }
+  
+  func testHashValues() {
+    XCTAssertNotEqual(o1.hashValue, o2.hashValue)
+  }
+  
+  func testEquatable() {
+    XCTAssertNotEqual(o1, o2)
+  }
+}
