a79d5ab5850829178d6a1430feafe42f808e2c94
Jordan Baird <jordanbaird.dev@gmail.com>
2022-10-22 22:39:17 -0600

    Make earliest supported platform macOS 10.13

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftKeys",
+    platforms: [
+        .macOS(.v10_13),
+    ],
     products: [
         .library(
             name: "SwiftKeys",
